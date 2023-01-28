973130be938ac83260cb4a7bed04da78f7cafbde
BaptisteCaulier <bcaulier@uclan.ac.uk>
2023-01-28 23:45:42 +0100

    Création de l'activité City

---
+++ app/src/main/java/com/example/projetmeteo/CityActivity.java
@@ -0,0 +1,247 @@
+package com.example.projetmeteo;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Objects;
+
+public class CityActivity extends AppCompatActivity
+{
+    //declaration des variables
+    private String URL;
+    private TextView textView_city;
+    private TextView textView_date;
+    private TextView textView_temperature;
+    private TextView textView_wind;
+    private TextView textView_air_quality;
+    private TextView textView_humidity;
+    private TextView textView_pressure;
+    private ImageView imageView_icon;
+    private Button button_notes;
+    private Button button_home;
+
+    //tableaux pour les pays, etats et villes
+    String [] countries = {"France","Germany","USA","Cyprus","Spain","Argentina","Australia","Canada","Cyprus","Germany","United Kingdom","France","Japan","China","Mexico","Germany","United Kingdom","USA","Spain","Poland","Turkey","Luxembourg","France","Cyprus","South Africa","Slovenia","Croatia","India","South Korea","USA","Australia","Canada","Japan","China","India","South Korea"};
+    String [] states = {"Ile-de-France","Berlin","California","Larnaka","Madrid","Buenos Aires","Queensland","Alberta","Nicosia","Bavaria","England","Hauts-de-France","Tokyo","Beijing","Mexico City","Bremen","Scotland","New York","Valencia","Lublin","Antalya","Diekirch","Nouvelle-Aquitaine","Pafos","Limpopo","Dolenjska","Zagreb","Delhi","Seoul","Texas","Victoria","Quebec","Akita","Fujian","Haryana","Daegu"};
+    String [] cities = {"Paris","Berlin","Los Angeles","Larnaca","Madrid","Buenos Aires","Brisbane","Brooks","Nicosia","Munich","London","Calais","Tokyo","Beijing","Mexico City","Bremen","Edinburgh","New York City","Valencia","Lublin","Antalya","Diekirch","Bordeaux","Paphos","Lephalale","Kromberk","Zagreb","Delhi","Seoul","Dallas","Melbourne","Montreal","Hiyama","Ximei","Palwal","Daegu"};
+
+    //variable pour recevoir la ville cliquée
+    int i = 50;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.city);
+
+        //on affecte l'id des éléments aux variables
+        this.textView_city = findViewById(R.id.city);
+        this.textView_date = findViewById(R.id.date);
+        this.textView_temperature = findViewById(R.id.temperature);
+        this.textView_wind = findViewById(R.id.wind);
+        this.textView_air_quality = findViewById(R.id.air_quality);
+        this.textView_humidity = findViewById(R.id.humidity);
+        this.textView_pressure = findViewById(R.id.pressure);
+
+        this.imageView_icon = findViewById(R.id.icon);
+
+        this.button_notes = findViewById(R.id.notes);
+        this.button_home = findViewById(R.id.home);
+
+        //on change la couleur des boutons
+        button_notes.setBackgroundColor(0XFF0072D7);
+        button_home.setBackgroundColor(0XFF0072D7);
+        button_notes.setVisibility(View.INVISIBLE);
+        button_home.setVisibility(View.INVISIBLE);
+
+        //on récupère la ville cliqué dans l'activité d'avant
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("item");
+        //si la valeur est nulle ça veut dire qu'on a cliqué sur le bouton ma position
+        if (value!=null)
+        {
+            //on récupère la ville et le pays
+            String sub1 = Objects.requireNonNull(value).substring(0,value.indexOf(","));
+            String sub2 = Objects.requireNonNull(value).substring(value.indexOf(","));
+            sub2 = sub2.substring(2);
+
+            //on cherche la bonne ville dans le tableau
+            if (Arrays.asList(cities).contains(sub1))
+            {
+                i = Arrays.asList(cities).indexOf(sub1);
+            }
+            if (Arrays.asList(cities).contains(sub2))
+            {
+                i = Arrays.asList(cities).indexOf(sub2);
+            }
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        //ici on regarde si on trouve la ville dans notre tableau
+        if (i!=50)
+        {
+            URL = "https://api.airvisual.com/v2/city?city=" + cities[i] + "&state=" + states[i] + "&country=" + countries[i] + "&key=83ecc71f-0909-4128-bef4-8be290278d12";
+        }
+        //sinon cela veut dire qu'on a appuyé sur le bouton ma position
+        else
+        {
+            URL = "https://api.airvisual.com/v2/nearest_city?key=83ecc71f-0909-4128-bef4-8be290278d12";
+        }
+
+        //on utilise la bibliothèque Volley pour appeler les données de l'API
+        RequestQueue requestQueue = Volley.newRequestQueue(CityActivity.this);
+
+        //on crée un objet JSON pour récupérer les données de l'API sous format JSON
+        @SuppressLint("SetTextI18n") JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, response -> {
+            try
+            {
+                JSONObject data = response.getJSONObject("data");
+                String city = data.getString("city");//city
+
+                JSONObject current = data.getJSONObject("current");
+                JSONObject weather = current.getJSONObject("weather");
+
+                //Affichage de la date au bon format
+                String ts = weather.getString("ts");
+                ts = ts.replace("T"," ");
+                ts = ts.replace("Z","");
+                DateTimeFormatter old_format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+                DateTimeFormatter good_format = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy", Locale.FRENCH);
+                LocalDateTime localDateTime = LocalDateTime.parse(ts, old_format);
+
+                int tp = weather.getInt("tp");//temperature
+                int pr = weather.getInt("pr");//pression atmospherqique
+                int hu = weather.getInt("hu");//humidité
+
+                //conversion du vent de m/s à km/h
+                double ws = weather.getDouble("ws");//vitesse du vent
+                ws = 3.6*ws;
+
+                //conversion de la direction du vent de degré à une direction
+                int wd = weather.getInt("wd");
+                String w_direction;
+                if (wd > 22 && wd < 68)
+                {
+                    w_direction = "NE";
+                }
+                else if (wd > 68 && wd < 113)
+                {
+                    w_direction = "E";
+                }
+                else if (wd > 113 && wd < 158)
+                {
+                    w_direction = "SE";
+                }
+                else if (wd > 158 && wd < 203)
+                {
+                    w_direction = "S";
+                }
+                else if (wd > 203 && wd < 248)
+                {
+                    w_direction = "SW";
+                }
+                else if (wd > 248 && wd < 293)
+                {
+                    w_direction = "W";
+                }
+                else if (wd > 293 && wd < 338)
+                {
+                    w_direction = "NW";
+                }
+                else
+                {
+                    w_direction = "N";
+                }
+
+                //on affiche le bon icone
+                String ic = weather.getString("ic");
+                if (!ic.equals("01d") && !ic.equals("01n") && !ic.equals("02d") && !ic.equals("02n") && !ic.equals("03d") && !ic.equals("04d") && !ic.equals("09d") && !ic.equals("10d") && !ic.equals("10n") && !ic.equals("11d") && !ic.equals("13d") && !ic.equals("50d"))
+                {
+                    ic = "01d";
+                }
+
+                JSONObject pollution = current.getJSONObject("pollution");
+                int aqius = pollution.getInt("aqius");//air quality index
+
+                //on affiche toutes les infos
+                textView_city.setText(city);
+                textView_city.setBackgroundColor(0X80FFFFFF);
+                textView_date.setText(good_format.format(localDateTime));
+                textView_date.setBackgroundColor(0X80FFFFFF);
+                textView_temperature.setText(tp + " °C");
+                textView_temperature.setBackgroundColor(0X80FFFFFF);
+                textView_wind.setText("Vent : " + (int) ws + " km/h, direction : " + w_direction);
+                textView_wind.setBackgroundColor(0X80FFFFFF);
+                textView_air_quality.setText("Qualité de l'air : " + aqius);
+                textView_air_quality.setBackgroundColor(0X80FFFFFF);
+                textView_humidity.setText("Humidité : " + hu + "%");
+                textView_humidity.setBackgroundColor(0X80FFFFFF);
+                textView_pressure.setText("Pression atmosphérique : " + pr + " hPa");
+                textView_pressure.setBackgroundColor(0X80FFFFFF);
+                imageView_icon.setBackgroundColor(0XB3FFFFFF);
+                imageView_icon.setImageDrawable(getResources().getDrawable(getResources().getIdentifier("@drawable/icon_" + ic, null, getPackageName())));
+                button_notes.setVisibility(View.VISIBLE);
+                button_home.setVisibility(View.VISIBLE);
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+                Toast.makeText(CityActivity.this, "Erreur", Toast.LENGTH_SHORT).show();
+            }
+        },
+                error -> Toast.makeText(CityActivity.this,"erreur",Toast.LENGTH_SHORT).show()
+        );
+
+        requestQueue.add(jsonObjectRequest);
+
+        //Le bouton accueil renvoie à la page d'accueil
+        button_home.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(CityActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Le bouton notes renvoie à l'activité notes créée prochainement
+        button_notes.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(CityActivity.this, "Ouverture des notes", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/projetmeteo/MainActivity.java
+++ app/src/main/java/com/example/projetmeteo/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.projetmeteo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,137 +9,155 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
+import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity {
-
-    //declaration des variables
+public class MainActivity extends AppCompatActivity
+{
+    //déclaration des variables
     Vector items = new Vector();
-    private Button ville;
-    private Button pays;
-    private Button favoris;
-    private Button position;
-    private ListView villesListView;
-    private ArrayAdapter villesArrayAdapter;
+    private Button city;
+    private Button country;
+    private Button favorites;
+    private Button location;
+    private ListView citiesListView;
+    private ArrayAdapter citiesArrayAdapter;
 
-    //declaration des tableaux pour les pays, états et pays
-    String[] tableaupays = {"France", "Germany", "USA", "Cyprus", "Spain", "Argentina", "Australia", "Canada", "Cyprus", "Germany", "United Kingdom", "France", "Japan", "China", "Mexico", "Germany", "United Kingdom", "USA", "Spain", "Poland", "Turkey", "Luxembourg", "France", "Cyprus", "South Africa", "Slovenia", "Croatia", "India", "South Korea", "USA", "Australia", "Canada", "Japan", "China", "India", "South Korea"};
-    String[] tableauetats = {"Ile-de-France", "Berlin", "California", "Larnaka", "Madrid", "Buenos Aires", "Queensland", "Alberta", "Nicosia", "Bavaria", "England", "Hauts-de-France", "Tokyo", "Beijing", "Mexico City", "Bremen", "Scotland", "New York", "Valencia", "Lublin", "Antalya", "Diekirch", "Nouvelle-Aquitaine", "Pafos", "Limpopo", "Dolenjska", "Zagreb", "Delhi", "Seoul", "Texas", "Victoria", "Quebec", "Akita", "Fujian", "Haryana", "Daegu"};
-    String[] tableauvilles = {"Paris", "Berlin", "Los Angeles", "Larnaca", "Madrid", "Buenos Aires", "Brisbane", "Brooks", "Nicosia", "Munich", "London", "Calais", "Tokyo", "Beijing", "Mexico City", "Bremen", "Edinburgh", "New York City", "Valencia", "Lublin", "Antalya", "Diekirch", "Bordeaux", "Paphos", "Lephalale", "Kromberk", "Zagreb", "Delhi", "Seoul", "Dallas", "Melbourne", "Montreal", "Hiyama", "Ximei", "Palwal", "Daegu"};
+    //tableaux pour les pays, etats et villes
+    String[] countries = {"France", "Germany", "USA", "Cyprus", "Spain", "Argentina", "Australia", "Canada", "Cyprus", "Germany", "United Kingdom", "France", "Japan", "China", "Mexico", "Germany", "United Kingdom", "USA", "Spain", "Poland", "Turkey", "Luxembourg", "France", "Cyprus", "South Africa", "Slovenia", "Croatia", "India", "South Korea", "USA", "Australia", "Canada", "Japan", "China", "India", "South Korea"};
+    String[] states = {"Ile-de-France", "Berlin", "California", "Larnaka", "Madrid", "Buenos Aires", "Queensland", "Alberta", "Nicosia", "Bavaria", "England", "Hauts-de-France", "Tokyo", "Beijing", "Mexico City", "Bremen", "Scotland", "New York", "Valencia", "Lublin", "Antalya", "Diekirch", "Nouvelle-Aquitaine", "Pafos", "Limpopo", "Dolenjska", "Zagreb", "Delhi", "Seoul", "Texas", "Victoria", "Quebec", "Akita", "Fujian", "Haryana", "Daegu"};
+    String[] cities = {"Paris", "Berlin", "Los Angeles", "Larnaca", "Madrid", "Buenos Aires", "Brisbane", "Brooks", "Nicosia", "Munich", "London", "Calais", "Tokyo", "Beijing", "Mexico City", "Bremen", "Edinburgh", "New York City", "Valencia", "Lublin", "Antalya", "Diekirch", "Bordeaux", "Paphos", "Lephalale", "Kromberk", "Zagreb", "Delhi", "Seoul", "Dallas", "Melbourne", "Montreal", "Hiyama", "Ximei", "Palwal", "Daegu"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //affectation des id des boutons aux variables
-        this.ville = findViewById(R.id.ville);
-        this.pays = findViewById(R.id.pays);
-        this.favoris = findViewById(R.id.favoris);
-        this.position = findViewById(R.id.position);
+        //on affecte l'id des boutons aux variables
+        this.city = findViewById(R.id.city);
+        this.country = findViewById(R.id.country);
+        this.favorites = findViewById(R.id.favorites);
+        this.location = findViewById(R.id.location);
 
-        //affectation de la listview et de l'arrayadapter
-        this.villesListView = findViewById(R.id.listView);
-        this.villesArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
-        villesListView.setAdapter(villesArrayAdapter);
+        //on change la couleur des boutons
+        city.setBackgroundColor(0XFF0072D7);
+        country.setBackgroundColor(0XFF0072D7);
+        favorites.setBackgroundColor(0XFF0072D7);
+        location.setBackgroundColor(0XFF0072D7);
+
+        //on affecte l'id de la listview à une variable et on l'adapte pour afficher une simple liste
+        this.citiesListView = findViewById(R.id.listView);
+        this.citiesArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+        citiesListView.setAdapter(citiesArrayAdapter);
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
-
-        //affichage des villes
-        for (int i=0; i<tableauvilles.length; i++)
-        {
-            items.add(tableauvilles[i] + ", " + tableaupays[i]);
+        //on affiche les villes dans la liste
+        for (int i = 0; i < cities.length; i++) {
+            items.add(cities[i] + ", " + countries[i]);
         }
 
         //action quand on clique sur une ville
-        villesListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        citiesListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
             {
-                //On affiche l'item cliqué
-                Toast.makeText(MainActivity.this, String.valueOf(items.get(i)), Toast.LENGTH_SHORT).show();
+                //on démarre l'activité CityActivity
+                Intent intent = new Intent(MainActivity.this, CityActivity.class);
+                //on garde l'item cliqué en paramètre
+                intent.putExtra("item", String.valueOf(items.get(i)));
+                startActivity(intent);
             }
         });
 
-        //action quand on clique sur le bouton tri par ville
-        ville.setOnClickListener(new View.OnClickListener()
+        //action quand le bouton tri par ville est cliqué
+        city.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
                 //on efface la liste
-                villesListView.setAdapter(null);
+                citiesListView.setAdapter(null);
                 items.clear();
-                villesListView.setAdapter(villesArrayAdapter);
+                citiesListView.setAdapter(citiesArrayAdapter);
 
-                //on copie le tableau dans un tableau temporaire
-                String[] copy = tableauvilles.clone();
+                //on clone le tableau des villes
+                String[] copy = cities.clone();
                 Arrays.sort(copy);
 
-                //on crée un tableau pour les clés
+                //on crée un tableau pour les clés des villes
                 int[] keys = new int[copy.length];
 
-                for (int i = 0; i < copy.length; i++)
-                {
+                for (int i = 0; i < copy.length; i++) {
                     int j = 0;
-                    //on boucle sur le tableau des villes et on ajoute la valeur de la ville quand la clé est la même
-                    while (!copy[i].equals(tableauvilles[j]))
-                    {
+                    //on boucle sur le tableau des villes et quand la valeur est égal à celle de la copie on met la clé correspondante dans le tableau des clés
+                    while (!copy[i].equals(cities[j])) {
                         j++;
                     }
                     keys[i] = j;
                 }
 
-                //on affiche la liste triée
-                for (int i = 0; i < tableauvilles.length; i++)
-                {
-                    items.add(copy[i] + ", " + tableaupays[keys[i]]);
+                //on affiche les villes dans l'ordre alphabétique
+                for (int i = 0; i < cities.length; i++) {
+                    items.add(copy[i] + ", " + countries[keys[i]]);
                 }
             }
         });
 
-        //action quand on clique sur le bouton tri par pays
-        pays.setOnClickListener(new View.OnClickListener() {
+        //action quand le bouton tri par pays est cliqué
+        country.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 //on efface la liste
-                villesListView.setAdapter(null);
+                citiesListView.setAdapter(null);
                 items.clear();
-                villesListView.setAdapter(villesArrayAdapter);
+                citiesListView.setAdapter(citiesArrayAdapter);
 
-                //on copie le tableau dans un tableau temporaire
-                String[] copy = tableaupays.clone();
+                //on clone le tableau des pays
+                String[] copy = countries.clone();
                 Arrays.sort(copy);
 
-                String[] tmp = tableaupays.clone();
+                String[] tmp = countries.clone();
 
-                //on crée un tableau pour les clés
+                //on crée un tableau pour les clés des pays
                 int[] keys = new int[copy.length];
 
-                for (int i = 0; i < copy.length; i++)
-                {
+                for (int i = 0; i < copy.length; i++) {
                     int j = 0;
-                    //on boucle sur le tableau des pays et on ajoute la valeur du pays quand la clé est la même
-                    while (!copy[i].equals(tmp[j]))
-                    {
+                    //n boucle sur le tableau des pays et quand la valeur est égal à celle de la copie on met la clé correspondante dans le tableau des clés
+                    while (!copy[i].equals(tmp[j])) {
                         j++;
                     }
                     keys[i] = j;
-                    //on met la valeur done quand on a fini le pays pour éviter les doubles
-                    tmp[j] = "done";
+                    //on met la valeur fait au pays qu'on a déjà visité pour éviter les doublons
+                    tmp[j] = "fait";
                 }
 
-                //on affiche la liste triée
-                for (int i = 0; i < tableaupays.length; i++)
-                {
-                    items.add(copy[i] + ", " + tableauvilles[keys[i]]);
+                //on affiche les pays dans l'ordre alphabétique
+                for (int i = 0; i < countries.length; i++) {
+                    items.add(copy[i] + ", " + cities[keys[i]]);
                 }
+            }
+        });
+
+        //action quand le bouton ma position est cliqué
+        location.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //on démarre l'activité CityActivity
+                Intent intent = new Intent(MainActivity.this, CityActivity.class);
+                startActivity(intent);
             }
         });
     }
